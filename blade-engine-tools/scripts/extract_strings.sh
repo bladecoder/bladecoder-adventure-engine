3459ef04512128a14365ec5a0eeb447d42436278
Rafael Garcia <bladecoder@gmail.com>
2014-09-01 15:12:42 +0200

    delete blade-engine-tools project

--- blade-engine-tools/scripts/extract_strings.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-PROJECT_MODEL=../venus-android/assets/model
-
-cd ..
-
-find "$PROJECT_MODEL" -regex ".*\.\xml" -exec java -cp ./bin org.bladecoder.i18n.ExtractStrings {} \;
--- blade-engine-tools/scripts/text2wav.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Voces disponibles en http://forja.guadalinex.org/frs/?group_id=21&release_id=120
-#echo $1 | iconv -f utf-8 -t iso-8859-1 |text2wave -o output.wav  -eval "(JuntaDeAndalucia_es_sf_diphone)"
-echo $1 | iconv -f utf-8 -t iso-8859-1 |text2wave -o /tmp/$2.wav  -eval "(voice_JuntaDeAndalucia_es_pa_diphone)"
-ffmpeg -i /tmp/$2.wav $2
-rm /tmp/$2.wav
