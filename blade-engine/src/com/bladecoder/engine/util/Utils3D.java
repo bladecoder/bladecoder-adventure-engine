ff284ed517dae9a9e65043cf901a692fede07e08
bladecoder <bladecoder@gmail.com>
2015-04-03 00:58:41 +0200

    fix deprecated warnings

--- blade-engine/src/com/bladecoder/engine/util/Utils3D.java
+++ blade-engine/src/com/bladecoder/engine/util/Utils3D.java
@@ -60,7 +60,7 @@ public final class Utils3D {
 		ModelBuilder modelBuilder = new ModelBuilder();
 		modelBuilder.begin();
 		MeshPartBuilder mpb = modelBuilder.part("parts", GL20.GL_TRIANGLES,
-				Usage.Position | Usage.Normal | Usage.Color, new Material(
+				Usage.Position | Usage.Normal | Usage.ColorUnpacked, new Material(
 						ColorAttribute.createDiffuse(Color.WHITE)));
 		mpb.setColor(1f, 1f, 1f, 1f);
 //		mpb.box(0, -0.1f, 0, 10, .2f, 10);
@@ -79,13 +79,13 @@ public final class Utils3D {
 	private static void createAxes() {
 		ModelBuilder modelBuilder = new ModelBuilder();
 		modelBuilder.begin();
-		MeshPartBuilder builder = modelBuilder.part("grid", GL20.GL_LINES, Usage.Position | Usage.Color, new Material());
+		MeshPartBuilder builder = modelBuilder.part("grid", GL20.GL_LINES, Usage.Position | Usage.ColorUnpacked, new Material());
 		builder.setColor(Color.LIGHT_GRAY);
 		for (float t = GRID_MIN; t <= GRID_MAX; t+=GRID_STEP) {
 			builder.line(t, 0, GRID_MIN, t, 0, GRID_MAX);
 			builder.line(GRID_MIN, 0, t, GRID_MAX, 0, t);
 		}
-		builder = modelBuilder.part("axes", GL20.GL_LINES, Usage.Position | Usage.Color, new Material());
+		builder = modelBuilder.part("axes", GL20.GL_LINES, Usage.Position | Usage.ColorUnpacked, new Material());
 		builder.setColor(Color.RED);
 		builder.line(0, 0, 0, 10, 0, 0);
 		builder.setColor(Color.GREEN);
