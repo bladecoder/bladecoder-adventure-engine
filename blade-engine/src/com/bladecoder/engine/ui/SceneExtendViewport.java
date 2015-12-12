5548d0d765fec37ccba644852a3e73a1c5b0c38a
rgarcia <bladecoder@gmail.com>
2015-12-11 20:36:25 -0600

    fix sceneextendviewport world size calc.

--- blade-engine/src/com/bladecoder/engine/ui/SceneExtendViewport.java
+++ blade-engine/src/com/bladecoder/engine/ui/SceneExtendViewport.java
@@ -41,50 +41,40 @@ public class SceneExtendViewport extends Viewport {
 	public SceneExtendViewport() {
 		setCamera(new OrthographicCamera());
 	}
-
+	
 	@Override
-	public void update(int screenWidth, int screenHeight, boolean centerCamera) {
-
-		Vector2 s = Scaling.fill.apply(getWorldWidth(), getWorldHeight(), screenWidth, screenHeight);
-
-		if (s.x > screenWidth)
-			setWorldWidth(minWorldWidth);
-		else
-			setWorldHeight(minWorldHeight);
-
+	public void update (int screenWidth, int screenHeight, boolean centerCamera) {
 		// Fit min size to the screen.
-		// super.setWorldSize(minWorldWidth, minWorldHeight);
-
-		Vector2 scaled = Scaling.fit.apply(getWorldWidth(), getWorldHeight(), screenWidth, screenHeight);
+		float worldWidth = minWorldWidth;
+		float worldHeight = minWorldHeight;
+		Vector2 scaled = Scaling.fit.apply(worldWidth, worldHeight, screenWidth, screenHeight);
 
 		// Extend in the short direction.
-		setScreenWidth(Math.round(scaled.x));
-		setScreenHeight(Math.round(scaled.y));
-
-		if (getScreenWidth() < screenWidth) {
-			float toViewportSpace = getScreenHeight() / getWorldHeight();
-			float toWorldSpace = getWorldHeight() / getScreenHeight();
-			float lengthen = (screenWidth - getScreenWidth()) * toWorldSpace;
-			if (maxWorldWidth > 0)
-				lengthen = Math.min(lengthen, maxWorldWidth - minWorldWidth);
-			setWorldWidth(getWorldWidth() + lengthen);
-			setScreenWidth(getScreenWidth() + Math.round(lengthen * toViewportSpace));
-		} else if (getScreenHeight() < screenHeight) {
-			float toViewportSpace = getScreenWidth() / getWorldWidth();
-			float toWorldSpace = getWorldWidth() / getScreenWidth();
-			float lengthen = (screenHeight - getScreenHeight()) * toWorldSpace;
-			if (maxWorldHeight > 0)
-				lengthen = Math.min(lengthen, maxWorldHeight - minWorldHeight);
-			setWorldHeight(getWorldHeight() + lengthen);
-			setScreenHeight(getScreenHeight() + Math.round(lengthen * toViewportSpace));
+		int viewportWidth = Math.round(scaled.x);
+		int viewportHeight = Math.round(scaled.y);
+		if (viewportWidth < screenWidth) {
+			float toViewportSpace = viewportHeight / worldHeight;
+			float toWorldSpace = worldHeight / viewportHeight;
+			float lengthen = (screenWidth - viewportWidth) * toWorldSpace;
+			if (maxWorldWidth > 0) lengthen = Math.min(lengthen, maxWorldWidth - minWorldWidth);
+			worldWidth += lengthen;
+			viewportWidth += Math.round(lengthen * toViewportSpace);
+		} else if (viewportHeight < screenHeight) {
+			float toViewportSpace = viewportWidth / worldWidth;
+			float toWorldSpace = worldWidth / viewportWidth;
+			float lengthen = (screenHeight - viewportHeight) * toWorldSpace;
+			if (maxWorldHeight > 0) lengthen = Math.min(lengthen, maxWorldHeight - minWorldHeight);
+			worldHeight += lengthen;
+			viewportHeight += Math.round(lengthen * toViewportSpace);
 		}
 
-		// Center
-		setScreenX((screenWidth - getScreenWidth()) / 2);
-		setScreenY((screenHeight - getScreenHeight()) / 2);
+		super.setWorldSize(worldWidth, worldHeight);
+
+		// Center.
+		setScreenBounds((screenWidth - viewportWidth) / 2, (screenHeight - viewportHeight) / 2, viewportWidth, viewportHeight);
 
 		apply(centerCamera);
-
+		
 		EngineLogger.debug("SCREEN VIEWPORT: " + getScreenWidth() + "x" + getScreenHeight());
 		EngineLogger.debug("SCREEN WORLD: " + getWorldWidth() + "x" + getWorldHeight());
 	}
