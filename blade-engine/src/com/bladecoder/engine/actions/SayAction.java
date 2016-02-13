5374dd38351143d78c16e7c82e7333501eb5a201
bladecoder <bladecoder@gmail.com>
2016-02-14 00:35:11 +0100

    Revert commit 158a817

--- blade-engine/src/com/bladecoder/engine/actions/SayAction.java
+++ blade-engine/src/com/bladecoder/engine/actions/SayAction.java
@@ -17,7 +17,10 @@ package com.bladecoder.engine.actions;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonValue;
 import com.bladecoder.engine.actions.Param.Type;
+import com.bladecoder.engine.anim.Tween;
 import com.bladecoder.engine.model.CharacterActor;
 import com.bladecoder.engine.model.InteractiveActor;
 import com.bladecoder.engine.model.Text;
@@ -47,6 +50,8 @@ public class SayAction extends BaseCallbackAction {
 	@ActionPropertyDescription("Queue the text if other text is showing, or show it immediately.")
 	private boolean queue = false;
 
+	private String previousAnim = null;
+
 	@Override
 	public boolean run(VerbRunner cb) {
 		float x, y;
@@ -54,8 +59,6 @@ public class SayAction extends BaseCallbackAction {
 
 		setVerbCb(cb);
 		InteractiveActor a = (InteractiveActor)World.getInstance().getCurrentScene().getActor(actor, false);
-		
-		boolean textWait = getWait();
 
 		if (soundId != null)
 			a.playSound(soundId);
@@ -70,18 +73,14 @@ public class SayAction extends BaseCallbackAction {
 				x = boundingRectangle.getX() + boundingRectangle.getWidth() / 2;
 				y = boundingRectangle.getY() + boundingRectangle.getHeight();
 				
-				CharacterActor ca = (CharacterActor)a;
+				color = ((CharacterActor)a).getTextColor();
 				
-				color = ca.getTextColor();
-				
-				ca.talk();
-				
-				// always wait to restore the 'stand' animation when TALK.
-				textWait = true;
+				restoreStandPose((CharacterActor)a);
+				startTalkAnim((CharacterActor)a);
 			}
 
 			World.getInstance().getTextManager().addText(text, x, y, queue, type, color, null,
-					textWait?this:null);
+					getWait()?this:null);
 		}
 
 		return getWait();
@@ -91,11 +90,41 @@ public class SayAction extends BaseCallbackAction {
 	public void resume() {
 		if (type == Text.Type.TALK) {
 			CharacterActor a = (CharacterActor) World.getInstance().getCurrentScene().getActor(actor, false);
-			a.stand();
+			a.startAnimation(previousAnim, Tween.Type.SPRITE_DEFINED, 0, null);
 		}
 
-		// after restore the 'stand' pose, only continue if the verb is waiting.
-		if(getWait())
-			super.resume();
+		super.resume();
 	}
+
+	private void restoreStandPose(CharacterActor a) {
+		if (a == null)
+			return;
+
+		String fa = a.getRenderer().getCurrentAnimationId();
+
+		// If the actor was already talking we restore the actor to the 'stand'
+		// pose
+		if (fa.startsWith(a.getTalkAnim())) {
+			a.stand();
+		}
+	}
+
+	private void startTalkAnim(CharacterActor a) {
+		previousAnim = a.getRenderer().getCurrentAnimationId();
+
+		a.talk();
+	}
+
+	@Override
+	public void write(Json json) {
+		json.writeValue("previousAnim", previousAnim);
+		super.write(json);
+	}
+
+	@Override
+	public void read(Json json, JsonValue jsonData) {
+		previousAnim = json.readValue("previousAnim", String.class, jsonData);
+		super.read(json, jsonData);
+	}
+
 }
--- blade-engine/src/com/bladecoder/engine/actions/SayDialogAction.java
+++ blade-engine/src/com/bladecoder/engine/actions/SayDialogAction.java
@@ -21,38 +21,48 @@ import com.badlogic.gdx.utils.JsonValue;
 import com.bladecoder.engine.model.BaseActor;
 import com.bladecoder.engine.model.CharacterActor;
 import com.bladecoder.engine.model.DialogOption;
+import com.bladecoder.engine.model.SpriteActor;
 import com.bladecoder.engine.model.Text;
 import com.bladecoder.engine.model.VerbRunner;
 import com.bladecoder.engine.model.World;
 import com.bladecoder.engine.util.EngineLogger;
 
-@ActionDescription("Says the selected option from the current dialog. This action does the next steps:\n"
-		+ "\n- Sets the player 'talk' animation and say the player text"
-		+ "\n- Puts the player 'stand' animation and sets the target actor 'talk' animation and says the response text"
-		+ "\n- Puts the target 'stand' animation")
+@ActionDescription("Says the selected option from the current dialog. This action does the next steps:\n" +
+"\n- Sets the player 'talk' animation and say the player text" +
+"\n- Restore the previous player animation and set the target actor 'talk' animation and say the response text" +
+"\n- Restore the target actor animation")
 public class SayDialogAction extends BaseCallbackAction {
 	private boolean characterTurn = false;
 	private String characterName;
 	private String responseText;
 
+	private String previousAnim;
+
 	@Override
 	public boolean run(VerbRunner cb) {
 		World w = World.getInstance();
-
-		if (w.getCurrentDialog() == null || w.getCurrentDialog().getCurrentOption() == null) {
+		
+		if(w.getCurrentDialog() == null || w.getCurrentDialog().getCurrentOption() == null) {
 			EngineLogger.debug("SayDialogAction WARNING: Current dialog doesn't found.");
-
+			
 			return false;
 		}
-
+		
 		setVerbCb(cb);
 		DialogOption o = w.getCurrentDialog().getCurrentOption();
 		String playerText = o.getText();
-
+		
 		responseText = o.getResponseText();
 		characterName = w.getCurrentDialog().getActor();
-
+		
 		characterTurn = true;
+		previousAnim = null;
+		
+		// If the player or the character is talking restore to 'stand' pose
+		restoreStandPose(w.getCurrentScene().getPlayer());
+		
+		if(w.getCurrentScene().getActor(characterName, false) instanceof SpriteActor)
+			restoreStandPose((CharacterActor)w.getCurrentScene().getActor(characterName, false));
 
 		if (playerText != null) {
 			CharacterActor player = w.getCurrentScene().getPlayer();
@@ -64,12 +74,15 @@ public class SayDialogAction extends BaseCallbackAction {
 			w.getTextManager().addText(playerText, x, y, false,
 					Text.Type.TALK, player.getTextColor(), null, this);
 
-			player.talk();
+			w.getTextManager()
+					.addText(playerText, player.getX(), player.getY() + player.getHeight(), false, Text.Type.TALK, player.getTextColor(), null, this);
+ 
+			startTalkAnim(player);
 
 		} else {
 			resume();
 		}
-
+		
 		return getWait();
 	}
 
@@ -78,12 +91,14 @@ public class SayDialogAction extends BaseCallbackAction {
 
 		World w = World.getInstance();
 		BaseActor actor = w.getCurrentScene().getActor(characterName, false);
-
+		
 		if (characterTurn) {
 			characterTurn = false;
-
-			CharacterActor player = World.getInstance().getCurrentScene().getPlayer();
-			player.stand();
+			
+			if(previousAnim!= null){
+				SpriteActor player = World.getInstance().getCurrentScene().getPlayer();
+				player.startAnimation(previousAnim, null);
+			}
 
 			if (responseText != null) {
 				Rectangle boundingRectangle = actor.getBBox().getBoundingRectangle();
@@ -94,23 +109,41 @@ public class SayDialogAction extends BaseCallbackAction {
 						y, false, Text.Type.TALK,
 						((CharacterActor) actor).getTextColor(), null, this);
 
-				if (actor instanceof CharacterActor) {
-					((CharacterActor) actor).talk();
+
+				if(actor instanceof CharacterActor) {
+					startTalkAnim((CharacterActor)actor);
 				}
 			} else {
 				super.resume();
 			}
 		} else {
-			if (actor instanceof CharacterActor) {
-				((CharacterActor) actor).stand();
+			if(actor instanceof SpriteActor) {
+				((SpriteActor)actor).startAnimation(previousAnim, null);
 			}
-			
-			super.resume();
+			super.resume();			
 		}
+	}
+	
+	private void restoreStandPose(CharacterActor a) {
+		if(a == null) return;
+		
+		String fa = a.getRenderer().getCurrentAnimationId();
+		
+		// If the actor was already talking we restore the actor to the 'stand' pose	
+		if(fa.startsWith(a.getTalkAnim())){ 		
+			a.stand();
+		}
+	}
+	
+	private void startTalkAnim(CharacterActor a) {
+		previousAnim = a.getRenderer().getCurrentAnimationId();
+		
+		a.talk();
 	}
 
 	@Override
 	public void write(Json json) {
+		json.writeValue("previousFA", previousAnim);
 		json.writeValue("responseText", responseText);
 		json.writeValue("characterTurn", characterTurn);
 		json.writeValue("characterName", characterName);
@@ -118,11 +151,13 @@ public class SayDialogAction extends BaseCallbackAction {
 	}
 
 	@Override
-	public void read(Json json, JsonValue jsonData) {
+	public void read (Json json, JsonValue jsonData) {
+		previousAnim = json.readValue("previousFA", String.class, jsonData);
 		responseText = json.readValue("responseText", String.class, jsonData);
 		characterTurn = json.readValue("characterTurn", boolean.class, false, jsonData);
 		characterName = json.readValue("characterName", String.class, jsonData);
 		super.read(json, jsonData);
 	}
+
 
 }
