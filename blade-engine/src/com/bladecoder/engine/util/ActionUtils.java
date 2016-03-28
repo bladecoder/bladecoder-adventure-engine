aebea318d4d40d556892b071cf6f6212d6c2e1ab
rgarcia <bladecoder@gmail.com>
2016-03-28 16:12:55 +0200

    More fault tolerant when loading actions: Action field not found.

--- blade-engine/src/com/bladecoder/engine/util/ActionUtils.java
+++ blade-engine/src/com/bladecoder/engine/util/ActionUtils.java
@@ -322,7 +322,9 @@ public class ActionUtils {
 						ActionUtils.setParam(action, v.name, null);
 					else
 						ActionUtils.setParam(action, v.name, v.asString());
-				} catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException e) {
+				} catch (NoSuchFieldException e) {
+					EngineLogger.debug("Action field not found - class: " + className + " field: " + v.name);
+				} catch (IllegalArgumentException | IllegalAccessException e) {
 					throw new SerializationException("Action: " + action.getClass().getCanonicalName() + "Param: " + v.name, e);
 				}
 			}
