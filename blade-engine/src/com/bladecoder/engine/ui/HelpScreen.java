679180a9562574cc6b694176a676d5a61b13dcf1
rgarcia <bladecoder@gmail.com>
2016-04-25 13:34:39 +0200

    Get current locale every time the help screen shows.

--- blade-engine/src/com/bladecoder/engine/ui/HelpScreen.java
+++ blade-engine/src/com/bladecoder/engine/ui/HelpScreen.java
@@ -31,6 +31,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.bladecoder.engine.assets.EngineAssetManager;
+import com.bladecoder.engine.i18n.I18N;
 import com.bladecoder.engine.ui.UI.Screens;
 import com.bladecoder.engine.ui.defaults.DefaultSceneScreen.UIModes;
 import com.bladecoder.engine.util.Config;
@@ -38,7 +39,11 @@ import com.bladecoder.engine.util.Config;
 public class HelpScreen extends ScreenAdapter implements BladeScreen {
 	private final static String PIE_FILENAME = "ui/helpPie";
 	private final static String TWO_BUTTONS_FILENAME = "ui/helpDesktop";
-	private final static String SINGLE_CLICK_FILENAME = "ui/helpDesktop"; // TODO Help for single button
+	private final static String SINGLE_CLICK_FILENAME = "ui/helpDesktop"; // TODO
+																			// Help
+																			// for
+																			// single
+																			// button
 
 	private Texture tex;
 
@@ -51,13 +56,14 @@ public class HelpScreen extends ScreenAdapter implements BladeScreen {
 		@Override
 		public boolean keyUp(int keycode) {
 			switch (keycode) {
-				case Input.Keys.ESCAPE:
-				case Input.Keys.BACK:
-					ui.setCurrentScreen(Screens.MENU_SCREEN);
-					break;
+			case Input.Keys.ESCAPE:
+			case Input.Keys.BACK:
+				ui.setCurrentScreen(Screens.MENU_SCREEN);
+				break;
 			}
 
-			// FIXME: This ALWAYS return true, even when we haven't dealt with the key. Is that expected?
+			// FIXME: This ALWAYS return true, even when we haven't dealt with
+			// the key. Is that expected?
 			return true;
 		}
 
@@ -93,6 +99,32 @@ public class HelpScreen extends ScreenAdapter implements BladeScreen {
 
 	@Override
 	public void show() {
+		final Locale locale = I18N.getCurrentLocale();
+		String filename = null;
+		UIModes uiMode = UIModes.valueOf(Config.getProperty(Config.UI_MODE, "TWO_BUTTONS").toUpperCase());
+
+		if (Gdx.input.isPeripheralAvailable(Peripheral.MultitouchScreen) && uiMode == UIModes.TWO_BUTTONS) {
+			uiMode = UIModes.PIE;
+		}
+
+		switch (uiMode) {
+		case PIE:
+			filename = PIE_FILENAME;
+			break;
+		case SINGLE_CLICK:
+			filename = SINGLE_CLICK_FILENAME;
+			break;
+		case TWO_BUTTONS:
+			filename = TWO_BUTTONS_FILENAME;
+			break;
+
+		}
+
+		localeFilename = MessageFormat.format("{0}_{1}.png", filename, locale.getLanguage());
+
+		if (!EngineAssetManager.getInstance().assetExists(localeFilename))
+			localeFilename = MessageFormat.format("{0}.png", filename);
+
 		tex = new Texture(EngineAssetManager.getInstance().getResAsset(localeFilename));
 		tex.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 
@@ -107,32 +139,5 @@ public class HelpScreen extends ScreenAdapter implements BladeScreen {
 	@Override
 	public void setUI(UI ui) {
 		this.ui = ui;
-
-		final Locale locale = Locale.getDefault();
-		String filename = null;
-		
-		UIModes uiMode = UIModes.valueOf(Config.getProperty(Config.UI_MODE, "TWO_BUTTONS").toUpperCase());
-		
-		if (Gdx.input.isPeripheralAvailable(Peripheral.MultitouchScreen) && uiMode == UIModes.TWO_BUTTONS) {
-			uiMode = UIModes.PIE;
-		}
-		
-		switch(uiMode) {
-		case PIE:
-			filename =  PIE_FILENAME;
-			break;
-		case SINGLE_CLICK:
-			filename = SINGLE_CLICK_FILENAME;
-			break;
-		case TWO_BUTTONS:
-			filename = TWO_BUTTONS_FILENAME;
-			break;
-		
-		}
-
-		localeFilename = MessageFormat.format("{0}_{1}.png", filename, locale.getLanguage());
-
-		if (!EngineAssetManager.getInstance().assetExists(localeFilename))
-			localeFilename = MessageFormat.format("{0}.png", filename);
 	}
 }
