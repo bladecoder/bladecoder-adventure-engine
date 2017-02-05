5ff74faa486a02d502f966c4bf2ddd8cf4155dde
bladecoder <bladecoder@gmail.com>
2017-02-05 22:15:36 +0100

    Debug mode when clicking 5 times in version label.

--- blade-engine/src/com/bladecoder/engine/ui/MenuScreen.java
+++ blade-engine/src/com/bladecoder/engine/ui/MenuScreen.java
@@ -28,6 +28,7 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -58,7 +59,7 @@ public class MenuScreen extends ScreenAdapter implements BladeScreen {
 	private Button credits;
 	private Button help;
 	private Button debug;
-	
+
 	private final Table menuButtonTable = new Table();
 	private final Table iconStackTable = new Table();
 
@@ -78,8 +79,8 @@ public class MenuScreen extends ScreenAdapter implements BladeScreen {
 	public void resize(int width, int height) {
 		stage.getViewport().update(width, height, true);
 		pointer.resize();
-		
-		float size = DPIUtils.getPrefButtonSize();	
+
+		float size = DPIUtils.getPrefButtonSize();
 		credits.setSize(size, size);
 		help.setSize(size, size);
 		debug.setSize(size, size);
@@ -114,11 +115,11 @@ public class MenuScreen extends ScreenAdapter implements BladeScreen {
 		if (bg == null && style.bgFile != null) {
 			bgTexFile = new Texture(EngineAssetManager.getInstance().getResAsset(style.bgFile));
 			bgTexFile.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-			
-			float scale = (float)bgTexFile.getHeight() / (float) stage.getViewport().getScreenHeight();
-			
-			int width = (int)(stage.getViewport().getScreenWidth() * scale);
-			int x0 = (int)((bgTexFile.getWidth() - width) / 2);
+
+			float scale = (float) bgTexFile.getHeight() / (float) stage.getViewport().getScreenHeight();
+
+			int width = (int) (stage.getViewport().getScreenWidth() * scale);
+			int x0 = (int) ((bgTexFile.getWidth() - width) / 2);
 
 			bg = new TextureRegionDrawable(new TextureRegion(bgTexFile, x0, 0, width, bgTexFile.getHeight()));
 		}
@@ -146,7 +147,7 @@ public class MenuScreen extends ScreenAdapter implements BladeScreen {
 
 			Label title = new Label(Config.getProperty(Config.TITLE_PROP, "Adventure Blade Engine"), skin,
 					style.titleStyle);
-			
+
 			title.setAlignment(Align.center);
 
 			menuButtonTable.add(title).padBottom(DPIUtils.getMarginSize() * 2);
@@ -190,31 +191,31 @@ public class MenuScreen extends ScreenAdapter implements BladeScreen {
 							}
 						}
 					};
-					
+
 					d.pad(DPIUtils.getMarginSize());
 					d.getButtonTable().padTop(DPIUtils.getMarginSize());
 					d.getButtonTable().defaults().padLeft(DPIUtils.getMarginSize()).padRight(DPIUtils.getMarginSize());
-					
-					Label l = new Label( I18N.getString("ui.override"), ui.getSkin(), "ui-dialog");
-				    l.setWrap(true);
-				    l.setAlignment(Align.center);
 
-				    d.getContentTable().add( l ).prefWidth( Gdx.graphics.getWidth() * .7f);
-					
+					Label l = new Label(I18N.getString("ui.override"), ui.getSkin(), "ui-dialog");
+					l.setWrap(true);
+					l.setAlignment(Align.center);
+
+					d.getContentTable().add(l).prefWidth(Gdx.graphics.getWidth() * .7f);
+
 					d.button(I18N.getString("ui.yes"), true, ui.getSkin().get("ui-dialog", TextButtonStyle.class));
 					d.button(I18N.getString("ui.no"), false, ui.getSkin().get("ui-dialog", TextButtonStyle.class));
 					d.key(Keys.ENTER, true).key(Keys.ESCAPE, false);
-					
+
 					d.show(stage);
 				} else {
 
 					try {
 						world.newGame();
-						ui.setCurrentScreen(Screens.SCENE_SCREEN);				
+						ui.setCurrentScreen(Screens.SCENE_SCREEN);
 					} catch (Exception e) {
 						EngineLogger.error("IN NEW GAME", e);
 						Gdx.app.exit();
-					}					
+					}
 				}
 			}
 		});
@@ -287,11 +288,43 @@ public class MenuScreen extends ScreenAdapter implements BladeScreen {
 		iconStackTable.setFillParent(true);
 		iconStackTable.pack();
 		stage.addActor(iconStackTable);
-		
-		Label version = new Label("v"+ Config.getProperty(Config.VERSION_PROP, " unspecified"), skin);
+
+		Label version = new Label("v" + Config.getProperty(Config.VERSION_PROP, " unspecified"), skin);
 		version.setPosition(DPIUtils.getMarginSize(), DPIUtils.getMarginSize());
+		version.addListener(new ClickListener() {
+			int count = 0;
+			long time = System.currentTimeMillis();
+
+			public void clicked(InputEvent event, float x, float y) {
+				if (System.currentTimeMillis() - time < 500) {
+					count++;
+				} else {
+					count = 0;
+				}
+
+				time = System.currentTimeMillis();
+
+				if (count == 4) {
+					EngineLogger.toggle();
+
+					if (World.getInstance().isDisposed())
+						return;
+
+					if (EngineLogger.debugMode()) {
+						iconStackTable.row();
+						iconStackTable.add(debug);
+					} else {
+						Cell<?> cell = iconStackTable.getCell(debug);
+						iconStackTable.removeActor(debug);
+						cell.reset();
+					}
+
+				}
+			}
+		});
+
 		stage.addActor(version);
-		
+
 		debug.addListener(new ClickListener() {
 			public void clicked(InputEvent event, float x, float y) {
 				DebugScreen debugScr = new DebugScreen();
@@ -305,19 +338,19 @@ public class MenuScreen extends ScreenAdapter implements BladeScreen {
 
 		Gdx.input.setInputProcessor(stage);
 	}
-	
+
 	protected Table getMenuButtonTable() {
 		return menuButtonTable;
 	}
-	
+
 	protected Table getIconStackTable() {
 		return iconStackTable;
 	}
-	
+
 	protected UI getUI() {
 		return ui;
 	}
-	
+
 	protected MenuScreenStyle getStyle() {
 		return ui.getSkin().get(MenuScreenStyle.class);
 	}
@@ -330,7 +363,7 @@ public class MenuScreen extends ScreenAdapter implements BladeScreen {
 	@Override
 	public void setUI(UI ui) {
 		this.ui = ui;
-		
+
 		menuButtonTable.setFillParent(true);
 		menuButtonTable.center();
 	}
