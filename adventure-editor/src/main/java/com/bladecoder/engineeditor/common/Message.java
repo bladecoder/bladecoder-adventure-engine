32f43318fa6ef90aa08734fb8d845d33fedba289
rgarcia <bladecoder@gmail.com>
2017-02-08 11:50:18 +0100

    Force messages to show in the glcontext thread.

--- adventure-editor/src/main/java/com/bladecoder/engineeditor/common/Message.java
+++ adventure-editor/src/main/java/com/bladecoder/engineeditor/common/Message.java
@@ -29,6 +29,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.Timer.Task;
 
 public class Message {
 	private static final float FADE_DURATION = 0.4f;
@@ -38,89 +40,111 @@ public class Message {
 
 	public static void init(Skin skin) {
 		msg = new Label("", skin, "message") {
-			
+
 			@Override
 			public Actor hit(float x, float y, boolean touchable) {
-				if(isModal)
+				if (isModal)
 					return this;
-				
+
 				return null;
 			}
 		};
-		
-		
+
 		Message.skin = skin;
 		msg.setWrap(true);
 		msg.setAlignment(Align.center, Align.center);
 	}
-	
-	public static void showMsg (Stage stage, String text) {
+
+	public static void showMsg(Stage stage, String text) {
 		showMsg(stage, text, false);
 	}
-	
-	public static void showMsg (Stage stage, String text, boolean modal) {
-		isModal = modal;
-		
-		if(text == null) {
-			hideMsg();
-			return;
-		}
 
-		add(stage, text);
-		
-		if (FADE_DURATION > 0) {
-			msg.getColor().a = 0;
-			msg.addAction(Actions.fadeIn(FADE_DURATION, Interpolation.fade));
-		}
+	public static void showMsg(final Stage stage, final String text, final boolean modal) {
+
+		Timer.post(new Task() {
+
+			@Override
+			public void run() {
+				isModal = modal;
+
+				if (text == null) {
+					hideMsg();
+					return;
+				}
+
+				add(stage, text);
+
+				if (FADE_DURATION > 0) {
+					msg.getColor().a = 0;
+					msg.addAction(Actions.fadeIn(FADE_DURATION, Interpolation.fade));
+				}
+
+			}
+		});
 	}
-	
+
 	private static void add(Stage stage, String text) {
 		msg.clearActions();
-		
+
 		msg.setText(text);
-		
+
 		GlyphLayout textLayout = new GlyphLayout();
-		
+
 		textLayout.setText(msg.getStyle().font, text, Color.BLACK, stage.getWidth() * .8f, Align.center, true);
-		
-		msg.setSize(textLayout.width + textLayout.height, textLayout.height  + textLayout.height * 2);
 
-		if(!stage.getActors().contains(msg, true))
+		msg.setSize(textLayout.width + textLayout.height, textLayout.height + textLayout.height * 2);
+
+		if (!stage.getActors().contains(msg, true))
 			stage.addActor(msg);
-		
-		msg.setPosition(Math.round((stage.getWidth() - msg.getWidth()) / 2), Math.round((stage.getHeight() - msg.getHeight()) / 2));
-		msg.invalidate();		
-	}
-	
-	public static void showMsg (Stage stage, String text, float duration) {
-		isModal = false;
-		
-		if(text == null) {
-			hideMsg();
-			return;
-		}
-		
-		add(stage, text);
-		
-		if (FADE_DURATION > 0) {
-			msg.getColor().a = 0;
-			msg.addAction(sequence(Actions.fadeIn(FADE_DURATION, Interpolation.fade), 
-					Actions.delay(duration, sequence(fadeOut(FADE_DURATION, Interpolation.fade), Actions.removeActor()))));
-		}
+
+		msg.setPosition(Math.round((stage.getWidth() - msg.getWidth()) / 2),
+				Math.round((stage.getHeight() - msg.getHeight()) / 2));
+		msg.invalidate();
 	}
 
-	public static void hideMsg () {
+	public static void showMsg(final Stage stage, final String text, final float duration) {
+
+		Timer.post(new Task() {
+
+			@Override
+			public void run() {
+				isModal = false;
+
+				if (text == null) {
+					hideMsg();
+					return;
+				}
+
+				add(stage, text);
+
+				if (FADE_DURATION > 0) {
+					msg.getColor().a = 0;
+					msg.addAction(sequence(Actions.fadeIn(FADE_DURATION, Interpolation.fade), Actions.delay(duration,
+							sequence(fadeOut(FADE_DURATION, Interpolation.fade), Actions.removeActor()))));
+				}
+
+			}
+		});
+	}
+
+	public static void hideMsg() {
 		isModal = false;
 		msg.setText("");
-		
+
 		if (FADE_DURATION > 0) {
 			msg.addAction(sequence(fadeOut(FADE_DURATION, Interpolation.fade), Actions.removeActor()));
 		}
-	}	
+	}
 
-	public static void showMsgDialog(Stage stage, String title, String msg) {
-		new Dialog(title, skin).text(msg).button("Close", true)
-				.key(Keys.ENTER, true)
-				.key(Keys.ESCAPE, false).show(stage);		
+	public static void showMsgDialog(final Stage stage, final String title, final String msg) {
+		Timer.post(new Task() {
+
+			@Override
+			public void run() {
+				new Dialog(title, skin).text(msg).button("Close", true).key(Keys.ENTER, true).key(Keys.ESCAPE, false)
+						.show(stage);
+
+			}
+		});
 	}
 }
