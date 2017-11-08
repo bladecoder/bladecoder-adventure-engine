81e778a2ffee4a5ea0d93a249771f9e066348acb
rgarcia <bladecoder@gmail.com>
2017-11-08 16:30:31 +0100

    Fix crash when screenshot of savegame doesn't exists.

--- blade-engine/src/com/bladecoder/engine/ui/LoadSaveScreen.java
+++ blade-engine/src/com/bladecoder/engine/ui/LoadSaveScreen.java
@@ -327,8 +327,13 @@ public class LoadSaveScreen extends ScreenAdapter implements BladeScreen {
 
 		if (EngineAssetManager.getInstance().getUserFile(filename).exists())
 			savedFile = EngineAssetManager.getInstance().getUserFile(filename);
-		else
+		else if (EngineAssetManager.getInstance().assetExists("tests/" + filename))
 			savedFile = EngineAssetManager.getInstance().getAsset("tests/" + filename);
+		else {
+			Drawable d = ui.getSkin().getDrawable("black");
+			
+			return new Image(d);
+		}
 
 		Texture t = new Texture(savedFile);
 
