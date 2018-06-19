fd51fdb02aae152192468e5aff5285ac24f9e7a2
rgarcia <bladecoder@gmail.com>
2018-06-19 12:28:45 +0200

    Fix copy/paste scene error.

--- adventure-editor/src/main/java/com/bladecoder/engineeditor/common/ElementUtils.java
+++ adventure-editor/src/main/java/com/bladecoder/engineeditor/common/ElementUtils.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
 import com.bladecoder.engine.actions.Action;
+import com.bladecoder.engine.model.Scene;
 import com.bladecoder.engine.serialization.BladeJson;
 import com.bladecoder.engine.serialization.BladeJson.Mode;
 import com.bladecoder.engine.util.ActionUtils;
@@ -49,6 +50,12 @@ public class ElementUtils {
 			EditorLogger.debug(str);
 			JsonValue root = new JsonReader().parse(str);
 			return ActionUtils.readJson(Ctx.project.getWorld(), json, root);
+		} else if (e instanceof Scene) {
+			String str = json.toJson(e, (Class<?>) null);
+			Scene s = new Scene(((Scene)e).getWorld());
+			JsonValue root = new JsonReader().parse(str);
+			s.read(json, root);
+			return s;
 		} else {
 			String str = json.toJson(e, (Class<?>) null);
 			return json.fromJson(e.getClass(), str);
