494b06cb87c025b09dfc6e6c1f10c1005f8e163f
Rafael Garcia <bladecoder@gmail.com>
2019-04-15 21:32:24 +0200

    Search inventories/uiactors on SetState action.

--- blade-engine/src/com/bladecoder/engine/actions/SetStateAction.java
+++ blade-engine/src/com/bladecoder/engine/actions/SetStateAction.java
@@ -21,7 +21,7 @@ import com.bladecoder.engine.model.VerbRunner;
 import com.bladecoder.engine.model.World;
 import com.bladecoder.engine.util.EngineLogger;
 
-@ActionDescription(name="ActorState", value="Sets the actor state")
+@ActionDescription(name = "ActorState", value = "Sets the actor state")
 public class SetStateAction implements Action {
 	@ActionProperty(required = true)
 	@ActionPropertyDescription("The target actor")
@@ -30,38 +30,34 @@ public class SetStateAction implements Action {
 	@ActionProperty
 	@ActionPropertyDescription("The actor 'state'")
 	private String state;
-	
+
 	private World w;
-	
+
 	@Override
 	public void init(World w) {
 		this.w = w;
 	}
 
 	@Override
-	public boolean run(VerbRunner cb) {			
+	public boolean run(VerbRunner cb) {
 		final Scene s = actor.getScene(w);
 
 		String actorId = actor.getActorId();
 		if (actorId == null) {
-			// if called in a scene verb and no actor is specified, set the state of the scene
+			// if called in a scene verb and no actor is specified, set the state of the
+			// scene
 			s.setState(state);
 			return false;
 		}
 
-		InteractiveActor a = (InteractiveActor)s.getActor(actorId, false);
-		
-		if(a == null) { // search in inventory
-			a = w.getInventory().get(actorId);
-		}
-		
-		if(a != null)
+		InteractiveActor a = (InteractiveActor) s.getActor(actorId, true);
+
+		if (a != null)
 			a.setState(state);
 		else
 			EngineLogger.error("SetState - Actor not found: " + actorId);
-		
+
 		return false;
 	}
-
 
 }
