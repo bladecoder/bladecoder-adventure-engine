fa319a5562e1db4f4143450a7f0fa2d05ba85233
rgarcia <bladecoder@gmail.com>
2019-07-09 15:08:32 +0200

    Animation button fixes.

--- blade-engine/src/com/bladecoder/engine/ui/InventoryButton.java
+++ blade-engine/src/com/bladecoder/engine/ui/InventoryButton.java
@@ -1,70 +1,68 @@
 package com.bladecoder.engine.ui;
 
-
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.bladecoder.engine.ui.InventoryUI.InventoryPos;
 import com.bladecoder.engine.util.DPIUtils;
 
-public class InventoryButton extends Button {
-	
+public class InventoryButton extends AnimButton {
+
 	private final InventoryUI inventory;
 	private final UI ui;
-	
+
 	private int numItems = Integer.MAX_VALUE;
 
 	public InventoryButton(UI ui, InventoryUI inv) {
 		super(ui.getSkin(), "inventory");
 		this.inventory = inv;
 		this.ui = ui;
-		
+
 		addListener(new ChangeListener() {
 			@Override
 			public void changed(ChangeEvent event, Actor actor) {
-				if(!inventory.isVisible())
+				if (!inventory.isVisible())
 					inventory.show();
 				else
 					inventory.hide();
 			}
 		});
 	}
-	
+
 	@Override
 	public void act(float delta) {
 		super.act(delta);
-		
-		if(numItems < ui.getWorld().getInventory().getNumItems()) {
+
+		if (numItems < ui.getWorld().getInventory().getNumItems()) {
 //			addAction(Actions.repeat(30, Actions.sequence(Actions.rotateBy(40, 1f), Actions.rotateBy(-40, 1f))));
 			addAction(Actions.repeat(4, Actions.sequence(Actions.moveBy(20, 0, .06f), Actions.moveBy(-20, 0, .06f))));
 //			addAction(Actions.repeat(3, Actions.sequence(Actions.alpha(0, .4f), Actions.alpha(1, .4f))));
 		}
-		
+
 		numItems = ui.getWorld().getInventory().getNumItems();
 	}
-	
+
 	public void resize(int width, int height) {
 		float size = DPIUtils.getPrefButtonSize();
 		float margin = DPIUtils.getMarginSize();
-		
+
 		setSize(size, size);
-		
+
 		InventoryPos inventoryPos = inventory.getInventoryPos();
-		
+
 		switch (inventoryPos) {
 		case TOP:
-			setPosition(margin, height - margin - getHeight());	
+			setPosition(margin, height - margin - getHeight());
 			break;
 		case LEFT:
-			setPosition(margin, margin);	
+			setPosition(margin, margin);
 			break;
 		case RIGHT:
-			setPosition(width - margin - getWidth(), margin);	
+			setPosition(width - margin - getWidth(), margin);
 			break;
 		case DOWN:
 		case CENTER:
-			setPosition(margin, margin);	
+			setPosition(margin, margin);
 			break;
 		default:
 			break;
