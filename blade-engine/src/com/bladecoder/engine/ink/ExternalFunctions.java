67acd62fff725ef007e3682962ab6123a58e8d30
Rafael Garcia <bladecoder@gmail.com>
2020-01-31 19:26:31 +0100

    More external functions.

--- blade-engine/src/com/bladecoder/engine/ink/ExternalFunctions.java
+++ blade-engine/src/com/bladecoder/engine/ink/ExternalFunctions.java
@@ -2,23 +2,20 @@ package com.bladecoder.engine.ink;
 
 import com.bladecoder.engine.actions.SceneActorRef;
 import com.bladecoder.engine.model.InteractiveActor;
+import com.bladecoder.engine.model.Inventory;
 import com.bladecoder.engine.model.Scene;
 import com.bladecoder.engine.model.World;
 import com.bladecoder.engine.util.EngineLogger;
+import com.bladecoder.ink.runtime.Story;
+import com.bladecoder.ink.runtime.Story.ExternalFunction0;
 import com.bladecoder.ink.runtime.Story.ExternalFunction1;
+import com.bladecoder.ink.runtime.Story.ExternalFunction2;
 
 public class ExternalFunctions {
 
-	private InkManager inkManager;
+	public static void bindExternalFunctions(final World w, Story story) throws Exception {
 
-	public ExternalFunctions() {
-	}
-
-	public void bindExternalFunctions(final World w, InkManager ink) throws Exception {
-
-		this.inkManager = ink;
-
-		inkManager.getStory().bindExternalFunction("inInventory", new ExternalFunction1<String, Boolean>() {
+		story.bindExternalFunction("inInventory", new ExternalFunction1<String, Boolean>() {
 
 			@Override
 			public Boolean call(String actor) throws Exception {
@@ -29,7 +26,7 @@ public class ExternalFunctions {
 			}
 		});
 
-		inkManager.getStory().bindExternalFunction("getActorState", new ExternalFunction1<String, String>() {
+		story.bindExternalFunction("getActorState", new ExternalFunction1<String, String>() {
 
 			@Override
 			public String call(String act) throws Exception {
@@ -49,7 +46,7 @@ public class ExternalFunctions {
 			}
 		});
 
-		inkManager.getStory().bindExternalFunction("getSceneState", new ExternalFunction1<String, String>() {
+		story.bindExternalFunction("getSceneState", new ExternalFunction1<String, String>() {
 
 			@Override
 			public String call(String scene) throws Exception {
@@ -61,6 +58,78 @@ public class ExternalFunctions {
 				}
 
 				return s.getState() == null ? "" : s.getState();
+			}
+		});
+
+		story.bindExternalFunction("getPlayer", new ExternalFunction0<String>() {
+
+			@Override
+			public String call() throws Exception {
+				return w.getCurrentScene().getPlayer().getId();
+			}
+		});
+
+		story.bindExternalFunction("inInventory2", new ExternalFunction2<String, String, Boolean>() {
+
+			@Override
+			public Boolean call(String actor, String inventory) throws Exception {
+				if (actor.charAt(0) == '>')
+					actor = actor.substring(1);
+
+				Inventory inv = w.getInventories().get(inventory);
+
+				if (inv == null) {
+					EngineLogger.debug("InkExternalFunction::inInventory2: Inventory not found: " + inventory);
+
+					return false;
+				}
+
+				return inv.get(actor) != null;
+			}
+		});
+
+		story.bindExternalFunction("getCurrentScene", new ExternalFunction0<String>() {
+
+			@Override
+			public String call() throws Exception {
+				return w.getCurrentScene().getId();
+			}
+		});
+
+		story.bindExternalFunction("isDebug", new ExternalFunction0<Boolean>() {
+
+			@Override
+			public Boolean call() throws Exception {
+				return EngineLogger.debugMode();
+			}
+		});
+
+		story.bindExternalFunction("getProperty", new ExternalFunction1<String, String>() {
+
+			@Override
+			public String call(String p) throws Exception {
+				String v = w.getCustomProperty(p);
+				return v == null ? "" : v;
+			}
+		});
+
+		story.bindExternalFunction("isVisible", new ExternalFunction1<String, Boolean>() {
+
+			@Override
+			public Boolean call(String act) throws Exception {
+				SceneActorRef actor = new SceneActorRef(act);
+				final Scene s = actor.getScene(w);
+
+				String actorId = actor.getActorId();
+
+				InteractiveActor a = (InteractiveActor) s.getActor(actorId, true);
+
+				if (a == null) {
+					EngineLogger.error("getActorState - Actor not found: " + actorId);
+					return false;
+				}
+
+				return a.isVisible();
 			}
 		});
 	}
