a79427bb5ce1c3f09fc23be2cb217c6072293416
Rafael García <bladecoder@gmail.com>
2020-05-27 09:49:18 +0200

    Fix bug when scaling atlases with subfolders.

--- adventure-editor/src/main/java/com/bladecoder/engineeditor/ui/CreateResolutionDialog.java
+++ adventure-editor/src/main/java/com/bladecoder/engineeditor/ui/CreateResolutionDialog.java
@@ -18,6 +18,7 @@ package com.bladecoder.engineeditor.ui;
 import java.io.File;
 import java.io.IOException;
 
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
@@ -37,76 +38,78 @@ public class CreateResolutionDialog extends EditDialog {
 	private static final String INFO = "Create a new resolution. Scale all atlases and images of the game.";
 
 	private InputPanel scale;
-	
+
 	protected ChangeListener listener;
-	
+
 	String atlasDir = Ctx.project.getAssetPath() + Project.ATLASES_PATH;
 	String uiDir = Ctx.project.getAssetPath() + Project.UI_PATH;
 	String imageDir = Ctx.project.getAssetPath() + Project.IMAGE_PATH;
 
 	public CreateResolutionDialog(Skin skin) {
 		super("CREATE RESOLUTION", skin);
-		
-		 scale = InputPanelFactory.createInputPanel(skin, "Scale",
-					"Scale relative to the world resolution", Param.Type.FLOAT, true);
+
+		scale = InputPanelFactory.createInputPanel(skin, "Scale", "Scale relative to the world resolution",
+				Param.Type.FLOAT, true);
 
 		addInputPanel(scale);
-		
+
 		setInfo(INFO);
 	}
 
 	@Override
 	protected void ok() {
-		
-		Message.showMsg(getStage(), "Creating resolution...", true);
-		
+
+		final Stage stage = getStage();
+
+		Message.showMsg(stage, "Creating resolution...", true);
+
 		Timer.schedule(new Task() {
 			@Override
-			public void run() {			
+			public void run() {
 				createResolution();
 
-				String msg = scaleImages();				
-				
-				if(listener != null)
+				String msg = scaleImages();
+
+				if (listener != null)
 					listener.changed(new ChangeEvent(), CreateResolutionDialog.this);
-				
+
 				Message.hideMsg();
-				
-				if(msg != null)
-					Message.showMsgDialog(getStage(), "Error creating resolution", msg);
+
+				if (msg != null)
+					Message.showMsgDialog(stage, "Error creating resolution", msg);
 			}
-		},1);
+		}, 1);
 	}
-	
+
 	private void createResolution() {
 //		float s = Float.parseFloat(scale.getText());
 //		String prefix = (int)(Ctx.project.getWorld().getWidth() * s) + "_" +  (int)(Ctx.project.getWorld().getHeight() * s);
 		String prefix = scale.getText().trim();
-		
+
 		new File(atlasDir + "/" + prefix).mkdir();
 		new File(uiDir + "/" + prefix).mkdir();
-		new File(imageDir + "/" + prefix).mkdir();		
+		new File(imageDir + "/" + prefix).mkdir();
 	}
 
 	private String scaleImages() {
-		
+
 		float s = Float.parseFloat(scale.getText());
 //		String prefix = (int)(Ctx.project.getWorld().getWidth() * s) + "_" +  (int)(Ctx.project.getWorld().getHeight() * s);
 		String prefix = scale.getText().trim();
-		
+
 		// COPY ASSETS FROM WORLD RESOLUTION SCALED
 		String wPrefix = Ctx.project.getResDir();
-		
+
 		try {
 			ImageUtils.scaleDirFiles(new File(uiDir + "/" + wPrefix), new File(uiDir + "/" + prefix), s);
 			ImageUtils.scaleDirFiles(new File(imageDir + "/" + wPrefix), new File(imageDir + "/" + prefix), s);
-			
+
 			ImageUtils.scaleDirAtlases(new File(atlasDir + "/" + wPrefix), new File(atlasDir + "/" + prefix), s);
 			ImageUtils.scaleDirAtlases(new File(uiDir + "/" + wPrefix), new File(uiDir + "/" + prefix), s);
 		} catch (IOException e) {
 			return e.getMessage();
 		}
-		
+
 		return null;
 	}
 
