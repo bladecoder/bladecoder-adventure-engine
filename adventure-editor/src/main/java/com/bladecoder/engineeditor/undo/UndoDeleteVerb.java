a6785af5e6b8625ec9ff50d5e7e4519db58455e4
Rafael García <bladecoder@gmail.com>
2020-06-12 21:30:25 +0200

    Fix delete verb undo + adquiring keyboard focus.

--- adventure-editor/src/main/java/com/bladecoder/engineeditor/undo/UndoDeleteVerb.java
+++ adventure-editor/src/main/java/com/bladecoder/engineeditor/undo/UndoDeleteVerb.java
@@ -5,15 +5,15 @@ import com.bladecoder.engine.model.VerbManager;
 import com.bladecoder.engineeditor.Ctx;
 import com.bladecoder.engineeditor.model.Project;
 
-
 public class UndoDeleteVerb implements UndoOp {
 	private VerbManager vm;
 	private Verb v;
-	
+
 	public UndoDeleteVerb(VerbManager vm, Verb v) {
 		this.vm = vm;
+		this.v = v;
 	}
-	
+
 	@Override
 	public void undo() {
 		vm.addVerb(v);
