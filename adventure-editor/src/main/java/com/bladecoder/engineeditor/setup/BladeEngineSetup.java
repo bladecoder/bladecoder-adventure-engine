25a1fb48427f9b5c119bd7f50cdbec2ab3c7c688
Rafael García <bladecoder@gmail.com>
2022-07-25 13:25:08 +0200

    delete project.properties from tmpl

--- adventure-editor/src/main/java/com/bladecoder/engineeditor/setup/BladeEngineSetup.java
+++ adventure-editor/src/main/java/com/bladecoder/engineeditor/setup/BladeEngineSetup.java
@@ -223,7 +223,6 @@ public class BladeEngineSetup {
         project.files.add(new ProjectFile("android/build.gradle", true));
         project.files.add(new ProjectFile("android/ic_launcher-web.png", false));
         project.files.add(new ProjectFile("android/proguard-rules.pro", false));
-        project.files.add(new ProjectFile("android/project.properties", true));
 
         if (sdkLocation != null)
             project.files.add(new ProjectFile("local.properties", true));
