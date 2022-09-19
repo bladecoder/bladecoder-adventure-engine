a4cc743740ca64ff888362283eb8fbfe2dc908fb
Rafael García <bladecoder@gmail.com>
2022-09-19 12:22:27 +0200

    Clear the current showing text if an empty text is showed

--- blade-engine/src/com/bladecoder/engine/actions/SayAction.java
+++ blade-engine/src/com/bladecoder/engine/actions/SayAction.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2014 Rafael Garcia Moreno.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,75 +28,80 @@ import com.bladecoder.engine.model.World;
 
 @ActionDescription("Says a text")
 public class SayAction implements Action {
-	@ActionPropertyDescription("The target actor.")
-	@ActionProperty(type = Type.CHARACTER_ACTOR, required = true)
-	private String actor;
+    @ActionPropertyDescription("The target actor.")
+    @ActionProperty(type = Type.CHARACTER_ACTOR, required = true)
+    private String actor;
 
-	@ActionPropertyDescription("The 'text' to show.")
-	@ActionProperty(type = Type.SMALL_TEXT)
-	private String text;
+    @ActionPropertyDescription("The 'text' to show.")
+    @ActionProperty(type = Type.SMALL_TEXT)
+    private String text;
 
-	@ActionPropertyDescription("The 'voice' file to play if selected.")
-	@ActionProperty(type = Type.VOICE)
-	private String voiceId;
+    @ActionPropertyDescription("The 'voice' file to play if selected.")
+    @ActionProperty(type = Type.VOICE)
+    private String voiceId;
 
-	@ActionProperty(required = true, defaultValue = "SUBTITLE")
-	@ActionPropertyDescription("The type of the text.")
-	private Text.Type type = Text.Type.SUBTITLE;
+    @ActionProperty(required = true, defaultValue = "SUBTITLE")
+    @ActionPropertyDescription("The type of the text.")
+    private Text.Type type = Text.Type.SUBTITLE;
 
-	@ActionPropertyDescription("The animation to set when talking.")
-	@ActionProperty(required = false)
-	private String animation;
+    @ActionPropertyDescription("The animation to set when talking.")
+    @ActionProperty(required = false)
+    private String animation;
 
-	@ActionPropertyDescription("The style to use (an entry in your `ui.json` in the `com.bladecoder.engine.ui.TextManagerUI$TextManagerUIStyle` section)")
-	@ActionProperty(type = Type.TEXT_STYLE, required = false)
-	private String style;
+    @ActionPropertyDescription("The style to use (an entry in your `ui.json` in the `com.bladecoder.engine.ui" +
+            ".TextManagerUI$TextManagerUIStyle` section)")
+    @ActionProperty(type = Type.TEXT_STYLE, required = false)
+    private String style;
 
-	@ActionProperty(defaultValue = "false")
-	@ActionPropertyDescription("Queue the text if other text is showing, or show it immediately.")
-	private boolean queue = false;
-	
-	@ActionProperty(required = true)
-	@ActionPropertyDescription("If this param is 'false' the text is showed and the action continues inmediatly")
-	private boolean wait = true;
-	
-	private World w;
-	
-	@Override
-	public void init(World w) {
-		this.w = w;
-	}
+    @ActionProperty(defaultValue = "false")
+    @ActionPropertyDescription("Queue the text if other text is showing, or show it immediately.")
+    private boolean queue = false;
 
-	@Override
-	public boolean run(VerbRunner cb) {
-		float x = TextManager.POS_SUBTITLE, y = TextManager.POS_SUBTITLE;
-		Color color = null;
+    @ActionProperty(required = true)
+    @ActionPropertyDescription("If this param is 'false' the text is showed and the action continues inmediatly")
+    private boolean wait = true;
 
-		if (text == null)
-			return false;
+    private World w;
 
-		InteractiveActor a = (InteractiveActor) w.getCurrentScene().getActor(actor, false);
+    @Override
+    public void init(World w) {
+        this.w = w;
+    }
 
-		if (type == Text.Type.TALK && a != null) {
-			Rectangle boundingRectangle = a.getBBox().getBoundingRectangle();
+    @Override
+    public boolean run(VerbRunner cb) {
+        float x = TextManager.POS_SUBTITLE, y = TextManager.POS_SUBTITLE;
+        Color color = null;
 
-			x = boundingRectangle.getX() + boundingRectangle.getWidth() / 2;
-			y = boundingRectangle.getY() + boundingRectangle.getHeight();
+        if (text == null) {
+            // if text is null, we stop showing the current text if any
+            while (w.getCurrentScene().getTextManager().getCurrentText() != null)
+                w.getCurrentScene().getTextManager().next();
+            return false;
+        }
 
-			color = ((CharacterActor) a).getTextColor();
-			
-			Vector2 talkingTextPos = ((CharacterActor) a).getTalkingTextPos();
-			
-			if(talkingTextPos != null) {
-				x += talkingTextPos.x;
-				y += talkingTextPos.y;
-			}
-		}
+        InteractiveActor a = (InteractiveActor) w.getCurrentScene().getActor(actor, false);
 
-		w.getCurrentScene().getTextManager().addText(text, x, y, queue, type, color, style,
-				a != null ? a.getId() : actor, voiceId, animation, wait?cb:null);
+        if (type == Text.Type.TALK && a != null) {
+            Rectangle boundingRectangle = a.getBBox().getBoundingRectangle();
 
-		return wait;
+            x = boundingRectangle.getX() + boundingRectangle.getWidth() / 2;
+            y = boundingRectangle.getY() + boundingRectangle.getHeight();
 
-	}
+            color = ((CharacterActor) a).getTextColor();
+
+            Vector2 talkingTextPos = ((CharacterActor) a).getTalkingTextPos();
+
+            if (talkingTextPos != null) {
+                x += talkingTextPos.x;
+                y += talkingTextPos.y;
+            }
+        }
+
+        w.getCurrentScene().getTextManager().addText(text, x, y, queue, type, color, style,
+                a != null ? a.getId() : actor, voiceId, animation, wait ? cb : null);
+
+        return wait;
+
+    }
 }
