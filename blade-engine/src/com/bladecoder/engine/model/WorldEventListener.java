9bae8dbe6676e4fb6a76e79bf9717af67eeda2b3
Rafael García <bladecoder@gmail.com>
2026-07-20 20:21:52 +0200

    chore: remove Apache License 2.0 headers from all source files

--- blade-engine/src/com/bladecoder/engine/model/WorldEventListener.java
+++ blade-engine/src/com/bladecoder/engine/model/WorldEventListener.java
@@ -1,9 +1,3 @@
-/*******************************************************************************
- * Copyright 2026 Rafael Garcia Moreno.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- *******************************************************************************/
 package com.bladecoder.engine.model;
 
 /** Optional observer for game events that are visible to a player. */
--- blade-engine/src/com/bladecoder/engine/remote/RemoteControlServer.java
+++ blade-engine/src/com/bladecoder/engine/remote/RemoteControlServer.java
@@ -1,9 +1,3 @@
-/*******************************************************************************
- * Copyright 2026 Rafael Garcia Moreno.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- ******************************************************************************/
 package com.bladecoder.engine.remote;
 
 import java.io.IOException;
